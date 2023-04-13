b05ec7e6eb73767b6c5612d37a2b575f0bb00599
Yulia Isaeva <pulinu@icloud.com>
2023-04-13 09:27:22 +0500

    add common-char-count solution

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(str1, str2) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  const result = [];
+  let map = new Map();
+
+  //проходимся по первой строке и записываем, сколько раз встречается каждая буква
+  for (let i = 0; i < str1.length; i++) {
+      const currentEl = str1[i];
+      if(map.has(currentEl)) {
+          //если уже есть, то к существующему значению + 1
+          map.set(currentEl, map.get(currentEl) + 1)
+      } else{
+          //если нет - просто 1
+          map.set(currentEl, 1)
+      }
+  }
+
+  //проходимся по второй строке и сравниваем с содержимым map
+  for (let i = 0; i < str2.length; i++) {
+      let currentEl2 = str2[i];
+      if(map.get(currentEl2)) {
+          //если элемент такой встречается, то уменьшаем map на единицу
+          map.set(currentEl2, map.get(currentEl2) - 1)
+          //и записываем это действие в массив, чтобы потом посчитать, сколько раз это произошло
+          result.push(currentEl2);
+      } 
+  }
+
+  //возвращаем число
+  return result.length;
 }
 
 module.exports = {
